f12112d4228e5e6ddd13a86023e094ce8c7d965b
Nanthicha Makjinda <nanthicha.mak@gmail.com>
2023-02-07 09:11:16 +0700

    refactor: restructure

---
+++ src/pages/user/chat.tsx
@@ -0,0 +1,12 @@
+import { NavBar } from "@/components/navbar";
+
+export const Chat = () => {
+  return (
+    <>
+      <NavBar />
+      <div className="w-full h-screen flex items-center justify-center text-3xl bg-backgroundColor">
+        Coming soon....
+      </div>
+    </>
+  );
+};
