c2274535286acba3400126d53f97c1ede13a417d
Nanthicha Makjinda <nanthicha.mak@gmail.com>
2023-02-08 19:13:43 +0700

    fix: change lib for dropdown menu

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,14 +1,15 @@
 import { getCurrentUser, logout } from "@/services/auth.service";
+import { Menu, Transition } from "@headlessui/react";
 import {
   BellIcon,
   ChatBubbleLeftEllipsisIcon,
+  ChevronDownIcon,
   HomeIcon,
   UserCircleIcon,
 } from "@heroicons/react/24/outline";
-import { Dropdown } from "@nextui-org/react";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useCallback } from "react";
+import { Fragment, useCallback } from "react";
 
 const navLinks = [
   {
@@ -32,7 +33,6 @@ const navLinks = [
 
 export const NavBar = () => {
   const router = useRouter();
-
   const profileMenu = [
     {
       name: "profile",
@@ -83,28 +83,39 @@ export const NavBar = () => {
         </ul>
         <div className="w-3/5 flex justify-end items-center">
           <div className="flex flex-col">
-            <Dropdown>
-              <Dropdown.Button light css={{ color: "white" }}>
-                <div className=" inline-flex items-center text-lg">
+            <Menu>
+              <Menu.Button>
+                <div className="inline-flex items-center text-lg text-white">
                   <UserCircleIcon className="h-6 w-6 mx-2" strokeWidth={2} />
                   {getCurrentUser().username ?? "Username"}
+                  <ChevronDownIcon className="h-6 w-6 mx-2" strokeWidth={2} />
                 </div>
-              </Dropdown.Button>
-              <Dropdown.Menu
-                onAction={(key) => profileMenu[key as number].onclick()}
+              </Menu.Button>
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
               >
-                {profileMenu.map((menu, index) => {
-                  return (
-                    <Dropdown.Item
-                      key={index}
-                      className={`hover:text-white hover:bg-${menu.color} hover:cursor-pointer py-2 px-8 my-1 rounded-xl`}
-                    >
-                      {menu.name}
-                    </Dropdown.Item>
-                  );
-                })}
-              </Dropdown.Menu>
-            </Dropdown>
+                <Menu.Items className="absolute right-5 z-10 mt-10 w-56 px-2 origin-top-right rounded-xl bg-white shadow-lg">
+                  {profileMenu.map((menu, index) => {
+                    return (
+                      <Menu.Item>
+                        <button
+                          className={`w-full text-start hover:text-white hover:bg-${menu.color} hover:cursor-pointer py-2 px-4 my-1 rounded-xl`}
+                          onClick={menu.onclick}
+                        >
+                          {menu.name}
+                        </button>
+                      </Menu.Item>
+                    );
+                  })}
+                </Menu.Items>
+              </Transition>
+            </Menu>
           </div>
         </div>
       </nav>
