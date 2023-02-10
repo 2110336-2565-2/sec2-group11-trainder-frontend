490a97e4424c2d7715824ce5e0c7814d0b649283
naphatbass <naphatbass@outlook.com>
2023-02-10 12:49:31 +0700

    added button
    - imported arrow icon
    - adjust font size
    - added button

--- src/pages/user/booking/profile.tsx
+++ src/pages/user/booking/profile.tsx
@@ -5,7 +5,7 @@ import { useCallback, useEffect, useState } from "react";
 import Link from "next/link";
 import { NavBar } from "@/components/navbar";
 import { UserProfile } from "@/services/user.service";
-
+import { ArrowLeftIcon } from "@heroicons/react/24/outline";
 const profile = () => {
   const router = useRouter();
   const default_profile:UserProfile ={
@@ -47,7 +47,7 @@ const profile = () => {
 
   const GenSkill = () =>{
     return(
-      <p className="text-start text-s mt-10 ml-10 mr-10 mb-5">
+      <p className="text-start text-xl mt-10 ml-10 mr-10 mb-5">
           Specialities : <br></br>
           Rating: <br></br>
           Training Fee: Baht <br></br>
@@ -61,12 +61,10 @@ const profile = () => {
     <main className="flex flex-col h-screen">
       <NavBar/>
     <div className="flex flex-row h-screen bg-white">
-        <div className="w-2/5 h-full bg-backgroundColor md:flex flex-col items-center justify-center">
-          <div className="md:flex flex-row m-[5%] items-center justify-center ">
-            <Link href="/user/booking/filter">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-7 h-7 mr-5">
-            <path fillRule="evenodd" d="M11.03 3.97a.75.75 0 010 1.06l-6.22 6.22H21a.75.75 0 010 1.5H4.81l6.22 6.22a.75.75 0 11-1.06 1.06l-7.5-7.5a.75.75 0 010-1.06l7.5-7.5a.75.75 0 011.06 0z" clipRule="evenodd" />
-            </svg>
+        <div className="w-2/5 h-full bg-backgroundColor md:flex flex-col items-center">
+          <div className="w-full md:flex flex-row items-center justify-center mt-10 mb-[10%] m-[5%] ">
+            <Link href="/user/booking/filter" className="mr-[5%]">
+              <ArrowLeftIcon className="w-7 h-7"></ArrowLeftIcon>
             </Link>
             <GenName/>
           </div>
@@ -77,6 +75,18 @@ const profile = () => {
           <GenSkill/>
           {/* <div className="p-4">{content}</div> */}
           <img src="/demo_map.png" className ="rounded-lg w-3/4 ml-10"></img>
+          <div className="flex flex-row mt-5 ml-10 space-x-[20%] w-3/4">
+            <Link href="/user/chat" className="ml-[15%]">
+              <button className="bg-pink text-gray-800 py-2 px-12 rounded shadow">
+                  Start Chat
+              </button>
+            </Link>
+            <Link href="">
+            <button className="bg-pink text-gray-800  py-2 px-12 rounded shadow ">
+                Calendar
+            </button>
+            </Link>
+          </div>
         </div>
     </div>
     </main>
