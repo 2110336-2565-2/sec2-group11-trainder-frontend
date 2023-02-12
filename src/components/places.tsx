214ed366f30f7a02e82e392b710be552f2db06e2
nopparuj917 <nopparuj.ta@gmail.com>
2023-02-12 21:47:04 +0700

    feat: Add address auto complete
    using the Google Maps Places API

---
+++ src/components/places.tsx
@@ -0,0 +1,45 @@
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+
+export default function Places() {
+  const {
+    ready,
+    value,
+    setValue,
+    suggestions: { status, data },
+    clearSuggestions,
+  } = usePlacesAutocomplete();
+
+  return (
+    <div>
+      <input
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        disabled={!ready}
+        type="text"
+        placeholder="Sub district, District, Province"
+        className="w-full pl-3.5 py-2.5 mt-2 mb-2 mx-2 block border border-gray rounded-xl"
+      />
+      <div>
+        {status === "OK" &&
+          data.map(({ description }) => (
+            <div
+              onClick={() => {
+                setValue(description, false);
+                clearSuggestions();
+                getGeocode({ address: description }).then((results) => {
+                  const { lat, lng } = getLatLng(results[0]);
+                  console.log("📍 Coordinates: ", { lat, lng });
+                });
+              }}
+              className="w-full pl-3.5 py-2.5 mt-2 mb-2 mx-2 block border border-gray rounded-xl text-black hover:cursor-pointer"
+            >
+              {description}
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+}
