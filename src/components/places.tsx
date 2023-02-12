c6b2b32c0c66fd5574e1039c587003587d9e24b8
nopparuj917 <nopparuj.ta@gmail.com>
2023-02-12 23:10:38 +0700

    refactor: extract onclick login into seperate function
    make code more human-friendly

--- src/components/places.tsx
+++ src/components/places.tsx
@@ -12,6 +12,15 @@ export default function Places() {
     clearSuggestions,
   } = usePlacesAutocomplete();
 
+  const onSuggestionClick = (description: string) => {
+    setValue(description, false);
+    clearSuggestions();
+    getGeocode({ address: description }).then((results) => {
+      const { lat, lng } = getLatLng(results[0]);
+      console.log("📍 Coordinates: ", { lat, lng });
+    });
+  };
+
   return (
     <div>
       <input
@@ -26,14 +35,8 @@ export default function Places() {
         {status === "OK" &&
           data.map(({ description }) => (
             <div
-              onClick={() => {
-                setValue(description, false);
-                clearSuggestions();
-                getGeocode({ address: description }).then((results) => {
-                  const { lat, lng } = getLatLng(results[0]);
-                  console.log("📍 Coordinates: ", { lat, lng });
-                });
-              }}
+              key={description}
+              onClick={() => onSuggestionClick(description)}
               className="w-full pl-3.5 py-2.5 mt-2 mb-2 mx-2 block border border-gray rounded-xl text-black hover:cursor-pointer"
             >
               {description}
