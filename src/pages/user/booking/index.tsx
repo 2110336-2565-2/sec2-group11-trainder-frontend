4ac45b9beab6b961d468826717d6d5a0c96d6a39
Ponrawoot <minnaja853@gmail.com>
2023-02-12 21:26:35 +0700

    edit filter bar

--- src/pages/user/booking/index.tsx
+++ src/pages/user/booking/index.tsx
@@ -12,8 +12,7 @@ import {
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { NavBar } from "@/components/navbar";
-import Rating from '@mui/material/Rating';
-
+import Rating from "@mui/material/Rating";
 
 const navLinks = [
   {
@@ -43,51 +42,50 @@ const TrainerFilter = () => {
         <NavBar></NavBar>
         <div className="grid grid-cols-2">
           <div className="bg-white box-border w-2/3 h-screen p-4 flex flex-col">
-            <div>
-              <button>FILTER</button>
+            <button className="flex">
+              <p>FILTER</p>
               <ChevronDownIcon
-                className="w-5 h-5 inline-flex mx-5"
+                className="w-5 h-5 relative -right-full -mx-20"
                 strokeWidth={3}
               ></ChevronDownIcon>
-              <hr className="my-2"/>
-            </div>
-            <div>
-              <button>SPECIALITY</button>
+            </button>
+            <hr className="my-2" />
+            <button className="flex">
+              <p>SPECIALITY</p>
               <ChevronUpIcon
-                className="w-5 h-5 inline-flex mx-5"
+                className="w-5 h-5 relative -right-3/4 mx-7"
                 strokeWidth={3}
               ></ChevronUpIcon>
-              {/* <Rating></Rating> */}
-              <hr className="my-2" />
-              <div className="box-border flex flex-row">
-                <label className="mx-1">
-                  <input type="checkbox" className="mx-1"/>
-                  Weight Loss (100)
-                </label>
-                <label className="mx-1">
-                  <input type="checkbox" className="mx-1"/>
-                  Rehabilitation (100)
-                </label>
-                <label className="mx-1">
-                  <input type="checkbox" className="mx-1"/>
-                  Weight Training (100)
-                </label>
-                <label className="mx-1">
-                  <input type="checkbox" className="mx-1"/>
-                  Yoga (100)
-                </label>
-              </div>
+            </button>
+            <hr className="my-2" />
+            <div className="box-border flex flex-row">
+              <label className="mx-1">
+                <input type="checkbox" className="mx-1" />
+                Weight Loss (100)
+              </label>
+              <label className="mx-1">
+                <input type="checkbox" className="mx-1" />
+                Rehabilitation (100)
+              </label>
+              <label className="mx-1">
+                <input type="checkbox" className="mx-1" />
+                Weight Training (100)
+              </label>
+              <label className="mx-1">
+                <input type="checkbox" className="mx-1" />
+                Yoga (100)
+              </label>
             </div>
-            <div>
-              <button className="my-2">TRAINING FEE</button>
+            <button className="flex">
+              <p>TRAINING FEE</p>
               <ChevronDownIcon
-                className="w-5 h-5 inline-flex mx-5"
+                className="w-5 h-5 relative -right-3/4 mx-1"
                 strokeWidth={3}
               ></ChevronDownIcon>
-              <hr className="my-2" />
-            </div>
+            </button>
+            <hr className="my-2" />
             <button className="bg-red-300 rounded-lg absolute bottom-20 mx-32 w-60 h-20 drop-shadow-lg">
-            <p className="text-xl text-white">Apply</p>
+              <p className="text-xl text-white">Apply</p>
             </button>
           </div>
           <div className="-mx-20">
@@ -104,15 +102,20 @@ const TrainerFilter = () => {
                 <p className="text-2xl font-semibold">OATORO</p>
                 <p>Specialities: </p>
                 <div className="flex flex-row">
-                <p>Rating:</p>
-                <Rating name="half-rating-read" defaultValue={3.5} precision={0.5} readOnly className="mx-2"/>
+                  <p>Rating:</p>
+                  <Rating
+                    name="half-rating-read"
+                    defaultValue={3.5}
+                    precision={0.5}
+                    readOnly
+                    className="mx-2"
+                  />
                 </div>
                 <p>Training fee:</p>
               </div>
               <ChevronRightIcon
                 className="w-10 h-10 mx-20 my-10"
                 strokeWidth={2}
-                
               ></ChevronRightIcon>
             </div>
             <div className="box-border  bg-white w-1/2 border-4 rounded-lg flex flex-row m-5 p-5 drop-shadow-lg">
@@ -121,8 +124,14 @@ const TrainerFilter = () => {
                 <p className="text-2xl font-semibold">ABCD</p>
                 <p>Specialities: </p>
                 <div className="flex flex-row">
-                <p>Rating:</p>
-                <Rating name="half-rating-read" defaultValue={3.5} precision={0.5} readOnly className="mx-2"/>
+                  <p>Rating:</p>
+                  <Rating
+                    name="half-rating-read"
+                    defaultValue={3.5}
+                    precision={0.5}
+                    readOnly
+                    className="mx-2"
+                  />
                 </div>
                 <p>Training fee:</p>
               </div>
