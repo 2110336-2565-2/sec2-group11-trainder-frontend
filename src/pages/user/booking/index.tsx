53df376250c4ee879685c8859ab6fc0878b2cab8
Ponrawoot <minnaja853@gmail.com>
2023-02-12 22:25:27 +0700

    edit css

--- src/pages/user/booking/index.tsx
+++ src/pages/user/booking/index.tsx
@@ -57,23 +57,23 @@ const TrainerFilter = () => {
                 strokeWidth={3}
               ></ChevronUpIcon>
             </button>
-            <hr className="my-2" />
-            <div className="box-border flex flex-row">
-              <label className="mx-1">
-                <input type="checkbox" className="mx-1" />
-                Weight Loss (100)
+            <hr className="my-2 " />
+            <div className="grid grid-cols-2 gap-2">
+              <label className="flex">
+                <input type="checkbox" className="mx-1 accent-teal-600"  />
+                <p className="text-sm">Weight Loss (100)</p>
               </label>
-              <label className="mx-1">
-                <input type="checkbox" className="mx-1" />
-                Rehabilitation (100)
+              <label className="flex">
+                <input type="checkbox" className="mx-1 accent-teal-600" />
+                <p className="text-sm">Rehabilitation (100)</p>
               </label>
-              <label className="mx-1">
-                <input type="checkbox" className="mx-1" />
-                Weight Training (100)
+              <label className="flex ">
+                <input type="checkbox" className="mx-1 accent-teal-600" />
+                <p className="text-sm">Weight Training (100)</p>
               </label>
-              <label className="mx-1">
-                <input type="checkbox" className="mx-1" />
-                Yoga (100)
+              <label className="flex">
+                <input type="checkbox" className="mx-1 accent-teal-600" />
+                <p className="text-sm">Yoga (100)</p>
               </label>
             </div>
             <button className="flex">
@@ -84,20 +84,20 @@ const TrainerFilter = () => {
               ></ChevronDownIcon>
             </button>
             <hr className="my-2" />
-            <button className="bg-red-300 rounded-lg absolute bottom-20 mx-32 w-60 h-20 drop-shadow-lg">
+            <button className="bg-red-300 rounded-lg absolute bottom-7 left-20 mx-20 w-60 h-12 drop-shadow-lg hover:bg-red-400">
               <p className="text-xl text-white">Apply</p>
             </button>
           </div>
-          <div className="-mx-20">
-            <div className="flex flex-row m-8 ">
-              <ArrowLeftIcon
+          <div className="absolute left-1/3 mx-20">
+            <div className="flex flex-row m-8 relative right-9">
+              <button><ArrowLeftIcon
                 className="w-8 h-8"
                 strokeWidth={3}
-              ></ArrowLeftIcon>
+              ></ArrowLeftIcon></button>
               <p className="text-2xl font-semibold mx-3">Choose your trainer</p>
             </div>
-            <div className="box-border bg-white w-1/2 border-4 rounded-lg flex flex-row m-5 p-5 drop-shadow-lg">
-              <div className="bg-slate-500 w-20 h-22 rounded-lg m-3"></div>
+            <div className="bg-white w-auto border-2 border-gray-400 rounded-3xl flex flex-row m-5 p-5 drop-shadow-lg hover:">
+              <div className="bg-gray-300 w-20 h-22 rounded-lg m-3"></div>
               <div className="flex flex-col m-2">
                 <p className="text-2xl font-semibold">OATORO</p>
                 <p>Specialities: </p>
@@ -113,13 +113,15 @@ const TrainerFilter = () => {
                 </div>
                 <p>Training fee:</p>
               </div>
+              <button>
               <ChevronRightIcon
-                className="w-10 h-10 mx-20 my-10"
+                className="w-10 h-10 mx-20 my-10 stroke-gray-500"
                 strokeWidth={2}
               ></ChevronRightIcon>
+              </button>
             </div>
-            <div className="box-border  bg-white w-1/2 border-4 rounded-lg flex flex-row m-5 p-5 drop-shadow-lg">
-              <div className="bg-slate-500 w-20 h-22 rounded-lg m-3"></div>
+            <div className=" bg-white w-auto border-2 border-gray-400 rounded-3xl flex flex-row m-5 p-5 drop-shadow-lg">
+              <div className="bg-gray-300 w-20 h-22 rounded-lg m-3"></div>
               <div className="flex flex-col m-2">
                 <p className="text-2xl font-semibold">ABCD</p>
                 <p>Specialities: </p>
@@ -135,10 +137,12 @@ const TrainerFilter = () => {
                 </div>
                 <p>Training fee:</p>
               </div>
+              <button>
               <ChevronRightIcon
-                className="w-10 h-10 mx-20 my-10"
+                className="w-10 h-10 mx-20 my-10 stroke-gray-500"
                 strokeWidth={2}
               ></ChevronRightIcon>
+              </button>
             </div>
           </div>
         </div>
