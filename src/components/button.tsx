3615c059d12cedec17d32a93f81c98692152ddc9
Nanthicha Makjinda <nanthicha.mak@gmail.com>
2023-02-21 10:24:28 +0700

    feat: add button disable props

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,6 +1,7 @@
 type ButtonProps = {
   name: string;
   onClick?: () => void;
+  disabled?: boolean;
   width?: string;
   margin?: string;
   type?: "button" | "submit" | "reset";
@@ -11,9 +12,14 @@ export const Button = (props: ButtonProps) => {
     <button
       className={`${props.width ?? "w-full"} py-2.5 px-3 ${
         props.margin ?? "my-3"
-      } bg-pink hover:bg-pink-dark shadow rounded-xl text-white`}
+      } ${
+        props.disabled
+          ? "bg-gray-light text-gray"
+          : "bg-pink hover:bg-pink-dark text-white"
+      } shadow rounded-xl`}
       type={props.type}
       onClick={props.onClick}
+      disabled={props.disabled ?? false}
     >
       {props.name}
     </button>
