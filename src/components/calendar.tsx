68fbd53fe4a110eb57075f4ee50602c6574a3689
nopparuj917 <nopparuj.ta@gmail.com>
2023-02-21 12:11:05 +0700

    fix(calendar): responsive on mobile

--- src/components/calendar.tsx
+++ src/components/calendar.tsx
@@ -9,10 +9,10 @@ export default function Calendar() {
   const [today, setToday] = useState(currentDate);
   const [selectDate, setSelectDate] = useState(currentDate);
   return (
-    <div className="flex gap-10 sm:divide-x justify-center sm:w-1/2 mx-auto pt-20 pb-16 sm:flex-row flex-col">
-      <div className="w-96 h-96 ">
+    <div className="flex flex-col gap-10 mx-auto pt-20 pb-16 sm:divide-x sm:w-1/2 sm:flex-row  justify-center items-center ">
+      <div className="w-96 h-100 ">
         <div className="flex justify-between items-center">
-          <h1 className="select-none font-semibold">
+          <h1 className="font-semibold">
             {months[today.month()]}, {today.year()}
           </h1>
           <div className="flex gap-10 items-center ">
@@ -73,7 +73,6 @@ export default function Calendar() {
             );
           })}
         </div>
-
         <div className=" grid grid-cols-7 ">
           {generateDate(today.month(), today.year()).map(
             ({ date, currentMonth, today }, index) => {
@@ -104,8 +103,8 @@ export default function Calendar() {
           )}
         </div>
       </div>
-      <div className="h-96 w-96 sm:px-5">
-        <h1 className=" font-semibold">
+      <div className="w-96 sm:px-5">
+        <h1 className="font-semibold">
           Schedule for {selectDate.toDate().toDateString()}
         </h1>
         <p className="text-gray-400">
