af08d70a725a8d66367c776f2b6ddfc66bba8c19
Ponrawoot <minnaja853@gmail.com>
2023-03-12 02:30:16 +0700

    edit modal

--- src/pages/user/review/[username].tsx
+++ src/pages/user/review/[username].tsx
@@ -1,5 +1,6 @@
 import { BackButton } from "@/components/backbutton";
 import { Button } from "@/components/button";
+// import { InputBox } from "@/components/inputBox";
 import { useState } from "react";
 
 const Review = () => {
@@ -15,7 +16,27 @@ const Review = () => {
       </div>
     );
   };
-
+  const RatingStar = () => {
+    return (
+      <div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="2"
+          stroke="currentColor"
+          aria-hidden="true"
+          className="h-10 w-10 none-yellow stroke-slate-300"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+          ></path>
+        </svg>
+      </div>
+    );
+  };
   const ReviewBox = () => {
     return (
       <div className="h-[20%] w-[80%] bg-white mb-[5%] border-2 border-gray rounded-3xl items-center justify-between drop-shadow-lg hover:bg-gray-light"></div>
@@ -60,46 +81,35 @@ const Review = () => {
               {showModal ? (
                 <>
                   <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-                    <div className="relative w-auto my-6 mx-auto max-w-3xl">
-                      {/*content*/}
-                      <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                        {/*header*/}
-                        <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                          <h3 className="text-3xl font-semibold">
-                            Modal Title
-                          </h3>
-                          <button
-                            className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                            onClick={() => setShowModal(false)}
-                          >
-                            <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                              ×
-                            </span>
-                          </button>
-                        </div>
-                        {/*body*/}
+                    <div className="relative  my-6 mx-auto w-1/2">
+                      <div className="border-0 rounded-2xl shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                         <div className="relative p-6 flex-auto">
-                          <p className="my-4 text-slate-500 text-lg leading-relaxed">
-                            Test
-                          </p>
+                          <div className="flex flex-col">
+                            <p className="text-xl text-black text-center">
+                              What is your rate ?
+                            </p>
+                            <div className="flex justify-center flex-row gap-2 my-2">
+                              <RatingStar />
+                              <RatingStar />
+                              <RatingStar />
+                              <RatingStar />
+                              <RatingStar />
+                            </div>
+                            <p className="text-xl text-black my-2">
+                              Add your review
+                            </p>
+                            <textarea className="w-full h-20 px-3 py-2 text-base text-gray-700 bg-slate-300 border rounded-lg focus:shadow-outline my-1"></textarea>
+                            <div className="flex items-center justify-end  border-solid border-slate-200 rounded-b">
+                          <div className="w-1/3">
+                            <Button
+                              name="submit"
+                              onClick={() => setShowModal(false)}
+                            />
+                          </div>
                         </div>
-                        {/*footer*/}
-                        <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
-                          <button
-                            className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                            type="button"
-                            onClick={() => setShowModal(false)}
-                          >
-                            Close
-                          </button>
-                          <button
-                            className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                            type="button"
-                            onClick={() => setShowModal(false)}
-                          >
-                            Submit
-                          </button>
+                          </div>
                         </div>
+
                       </div>
                     </div>
                   </div>
