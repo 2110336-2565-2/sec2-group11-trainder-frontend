b214c5a2491e0ba5204b84e2a2474ccce8fab5da
naphatbass <naphatbass@outlook.com>
2023-03-12 00:29:27 +0700

    update UI
    - added component to review box (starRating and comment) ready for apply value from api
    TODO : get review api is not working , clean up code

--- src/pages/user/review/[username].tsx
+++ src/pages/user/review/[username].tsx
@@ -1,9 +1,54 @@
 import { BackButton } from "@/components/backbutton";
 import { Button } from "@/components/button";
-import { useState } from "react";
+import { useState,useEffect } from "react";
+import { UserProfile } from "@/services/user.service";
+import { getTrainerProfile, TrainerProfile } from "@/services/trainer.service";
+import { useRouter } from "next/router";
+import { getTrainerReviews, ReviewDetail } from "@/services/trainer.service";
+import {StarIcon} from "@heroicons/react/24/outline";
 
 const Review = () => {
   const [showModal, setShowModal] = useState(false);
+
+  const [trainerProfile, setTrainerProfile] = useState<UserProfile>({
+    username: "user",
+    firstname: "firstname",
+    lastname: "lastname",
+    birthdate: new Date(),
+    citizenId: "none",
+    gender: "none",
+    phoneNumber: "none",
+    address: "none",
+    usertype: "none",
+    lat: 0,
+    lng: 0,
+  });
+
+  
+
+  const [trainerReviews, setTrainerReviews] = useState<ReviewDetail[]>([]);
+
+  const router = useRouter();
+  const { username } = router.query;
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+  if (typeof username == "string") {
+    getTrainerProfile(username)
+      .then((res1) => {
+        setTrainerProfile(res1.userProfile);
+      })
+      .catch(() => router.back())
+    // getTrainerReviews(username)
+    //   .then((res2) => {
+    //     setTrainerReviews(res2.reviewList);
+    //   })
+    // .catch(() => router.back())
+  } else {
+    router.back();
+  }
+}, [username]);
+
   const Image = () => {
     return (
       <div className="max-h-min overflow-hidden mt-6 md:mt-10 flex justify-center">
@@ -16,11 +61,47 @@ const Review = () => {
     );
   };
 
-  const ReviewBox = () => {
+  const StarRating = (rating:{ rating: number; }) => {
+    return(
+      <div className="flex flex-row">
+        {[...Array<Number>(Math.round(rating.rating)),].map((e, idx) => {
+          return (
+            <StarIcon
+              key={idx}
+              className="h-6 w-6 fill-yellow stroke-yellow"
+              strokeWidth={2}
+            ></StarIcon>
+          );
+        })}
+        {[...Array<Number>(5 -Math.round(rating.rating)),].map((e, idx) => {
+          return (
+            <StarIcon
+              key={idx}
+              className="h-6 w-6 fill-none stroke-yellow"
+              strokeWidth={2}
+            ></StarIcon>
+          );
+          })}
+      </div>
+    )
+  }
+
+  const ReviewBox = (review:ReviewDetail) => {
     return (
-      <div className="h-[20%] w-[80%] bg-white mb-[5%] border-2 border-gray rounded-3xl items-center justify-between drop-shadow-lg hover:bg-gray-light"></div>
+      <div className="h-[20%] w-[80%] bg-white mb-[5%] border-2 border-gray rounded-3xl items-center justify-between drop-shadow-lg hover:bg-gray-light">
+        <div className="m-5">
+          <div className="text-lg">
+            <StarRating rating={review.rating}></StarRating>
+          </div>
+          <div className="text-lg">
+            comment : {review.comment}
+          </div>
+        </div>  
+      </div>
     );
   };
+
+  
 
   return (
     <>
@@ -34,7 +115,7 @@ const Review = () => {
             </p>
           </div>
           <div className="mt-[15%] flex flex-col items-center justify-start px-2 md:px-5">
-            Trainer Name
+            {trainerProfile.firstname} {trainerProfile.lastname}
             <Image />
           </div>
         </div>
@@ -42,13 +123,13 @@ const Review = () => {
           <div className="mt-[15%]"></div>
           <div className="flex-grow overflow-auto">
             <div className="h-full overflow-auto scrollbar-thin scrollbar-thumb-white-300 scrollbar-track-transparent">
-              <ReviewBox />
-              <ReviewBox />
-              <ReviewBox />
-              <ReviewBox />
-              <ReviewBox />
-              <ReviewBox />
-              <ReviewBox />
+              <ReviewBox comment="default comment" createdAt="default address" rating={5} username="default name"/>
+              <ReviewBox comment="default comment" createdAt="default address" rating={4.5} username="default name"/>
+              <ReviewBox comment="default comment" createdAt="default address" rating={4} username="default name"/>
+              <ReviewBox comment="default comment" createdAt="default address" rating={3.5} username="default name"/>
+              <ReviewBox comment="default comment" createdAt="default address" rating={3} username="default name"/>
+              <ReviewBox comment="default comment" createdAt="default address" rating={2.5} username="default name"/>
+              <ReviewBox comment="default comment" createdAt="default address" rating={2} username="default name"/>
             </div>
           </div>
           <div className="flex justify-end w-full space-x-10 text-start text-lg md:text-xl ">
@@ -73,7 +154,7 @@ const Review = () => {
                             onClick={() => setShowModal(false)}
                           >
                             <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                              ×
+                              
                             </span>
                           </button>
                         </div>
