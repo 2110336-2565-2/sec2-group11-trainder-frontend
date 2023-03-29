2d20f1cd0df025686727495c5334beeca10c75ad
naphatbass <naphatbass@outlook.com>
2023-03-29 18:17:08 +0700

    Add modal
    - added modal popup to add chat button
    - applied props to ChatBox
    - applied props to TrainerList in popup

--- src/pages/user/chat.tsx
+++ src/pages/user/chat.tsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
-import { PlusIcon } from "@heroicons/react/24/outline";
+import { PlusIcon, XMarkIcon} from "@heroicons/react/24/outline";
 import Image from "next/image";
 
-const ChatBox = () => {
+const ChatBox = (props:{name:string,message:string}) => {
   const [isClicked, setIsClicked] = useState(false);
 
   const handleChatBoxClick = () => {
@@ -26,9 +26,9 @@ const ChatBox = () => {
         />
       </div>
       <div className="h-full w-full flex-col mt-[2%] ml-[5%]">
-        <p className="text-xl m-[2%]">Name Surname</p>
+        <p className="text-xl m-[2%]">{props.name}</p>
         <p className="text-gray m-[2%]">
-          message : last sentence of the chat
+          message : {props.message}
         </p>
       </div>
     </div>
@@ -36,9 +36,29 @@ const ChatBox = () => {
 };
 
 const Chat = () => {
+  const [showModal, setShowModal] = useState(false);
+
   const handleAddChatClick = () => {
-    // Implement the logic to add a new chat
+    setShowModal(true);
   };
+
+  const handleCloseModalClick = () => {
+    setShowModal(false);
+  };
+
+  const handleTrainerSelectModalClick = () => {
+    setShowModal(false);
+  };
+
+  const TrainerListBox =(props:{name:string}) => {
+    return(
+      <button onClick={handleTrainerSelectModalClick}>
+        <div className="text-xl p-[2%] text-gray hover:bg-gray-200">
+          {props.name}
+        </div>
+      </button>
+    )
+  }
 
   return (
     <main className="w-full h-screen pt-20 flex flex-col overflow-hidden ">
@@ -47,21 +67,37 @@ const Chat = () => {
           <p className="text-xl md:text-3xl font-bold ml-[2%] mt-[2%]">
             Chats
           </p>
-          <button onClick={handleAddChatClick}>
-            <PlusIcon className="w-10 m-[2%]" />
+          <button onClick={handleAddChatClick} className="m-[2%]">
+            <PlusIcon className="w-10" />
           </button>
         </div>
         <div className="h-full w-full overflow-y-scroll">
-          <ChatBox />
-          <ChatBox />
-          <ChatBox />
-          <ChatBox />
-          <ChatBox />
-          <ChatBox />
-          <ChatBox />
-          <ChatBox />
+          <ChatBox name="Lalisa Manobal" message="last message"/>
+          <ChatBox name="Jennie Kim" message="last message"/>
+          <ChatBox name="Jisoo Kim" message="last message"/>
+          <ChatBox name="Chaeyong Pak" message="last message"/>
         </div>
       </div>
+      {showModal && (
+        <div className="fixed top-0 left-0 w-full h-full bg-gray-500 bg-opacity-50 z-50 flex flex-row justify-center items-center">
+          <div className="flex flex-col bg-white p-8 rounded-md w-1/2 h-1/2">
+            <div className=" flex flex-row justify-between">
+              <p className="text-xl">Add new chat</p>
+              <button onClick={handleCloseModalClick} className="w-5 h-5">
+                <XMarkIcon className="w-5 h-5"/>
+              </button>
+            </div>
+            <div className="flex flex-col overflow-y-scroll">
+              <TrainerListBox name="Soo JeeKim"/>
+              <TrainerListBox name="Salisa Slytherin"/>
+              <TrainerListBox name="Soo JeeKim"/>
+              <TrainerListBox name="Salisa Slytherin"/>
+              <TrainerListBox name="Soo JeeKim"/>
+              <TrainerListBox name="Salisa Slytherin"/>
+            </div> 
+          </div>
+        </div>
+      )}
     </main>
   );
 };
