bdfef6b54ff9a058c44b511b38c9d301b2d0d49d
Ponrawoot <minnaja853@gmail.com>
2023-03-31 00:19:56 +0700

    change color and set layout

--- src/pages/user/chat.tsx
+++ src/pages/user/chat.tsx
@@ -6,7 +6,7 @@ const Chat = () => {
       <div className="grid justify-items-end">
         <div className="p-5 flex flex-row items-end">
           <p className="px-1 text-xs">{props.time}</p>
-          <div className="bg-white h-8 rounded-lg p-1 drop-shadow-2xl">
+          <div className="bg-[#5D86BC] h-8 rounded-xl p-1 drop-shadow-2xl">
             <p>{props.message}</p>
           </div>
         </div>
@@ -17,7 +17,7 @@ const Chat = () => {
   const ContactPersonMessage = (props: { message: string; time: string }) => {
     return (
       <div className="p-5 flex flex-row items-end">
-        <div className="bg-[#5D86BC] h-8 rounded-lg p-1 drop-shadow-2xl">
+        <div className="bg-white h-8 rounded-xl p-1 drop-shadow-2xl">
           <p>{props.message}</p>
         </div>
         <p className="px-1 text-xs">{props.time}</p>
@@ -30,7 +30,7 @@ const Chat = () => {
       <div className="flex flex-row">
         <div className="flex flex-col w-2/5 h-screen bg-[#FCF4F1]"></div>
         <div className="flex flex-col w-3/5 h-screen bg-[#F1EFEF] relative">
-          <div className="pt-5 flex flex-row items-center">
+          <div className="px-5 pt-8 flex flex-row items-center">
             <div className=" ml-[1%] rounded-full overflow-hidden">
               <Image
                 src="/default_profile.jpg"
@@ -43,7 +43,7 @@ const Chat = () => {
             <p className="px-2">Lalisa Manoban</p>
           </div>
 
-          <div className="absolute bottom-16 w-full overflow-auto">
+          <div className="absolute bottom-12 w-full overflow-auto">
             <ContactPersonMessage message="Hello" time="14.50" />
             <UserMessage message="..." time="15.00" />
             <UserMessage message="Some Chat ..." time="15.00" />
@@ -53,9 +53,9 @@ const Chat = () => {
             />
             <ContactPersonMessage message="This is short text" time="15.06"/>
           </div>
-          <div className="p-2 bg-white absolute bottom-16 w-full ">
+          <div className="p-2 bg-white absolute bottom-14 w-full ">
             <input
-              className="p-2 rounded-3xl w-full border bg-gray-300  "
+              className="p-2 rounded-3xl w-full border bg-[#F5F2F2]"
               type="text"
               placeholder="Aa"
             />
